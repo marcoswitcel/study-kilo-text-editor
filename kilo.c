fab0cd07631650f31bb724768f6f40d32aa88e16
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-04-24 14:29:16 -0300

    comentários

--- kilo.c
+++ kilo.c
@@ -3,11 +3,17 @@
 #include <unistd.h>
 
 struct termios orig_termios;
+
 void disableRawMode() {
+  // This is because of the TCSAFLUSH option being passed to tcsetattr() when the program exits
+  // it discards any unread input before applying the changes to the terminal
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
+
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
+  // atexit() comes from <stdlib.h>.
+  // We use it to register our disableRawMode() function to be called automatically when the program exits
   atexit(disableRawMode);
   struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
