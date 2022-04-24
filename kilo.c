9f74d3dcdaeaf167a3323e6892ab4185a42f22e2
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-04-24 14:48:17 -0300

    visualizando teclas e desligando sigint

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -16,7 +18,7 @@ void enableRawMode() {
   // We use it to register our disableRawMode() function to be called automatically when the program exits
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -25,6 +27,12 @@ int main() {
   enableRawMode();
 
   char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
